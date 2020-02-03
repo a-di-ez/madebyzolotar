dd6480805defc33e0015daec2d2cf97ae3d84f1e
Vadim Osochenko <vadim@magpaylabs.com>
2020-02-03 22:49:04 +0200

    UP: clean MAP

--- public/js/front.js
+++ public/js/front.js
@@ -3,7 +3,6 @@ $(function () {
     lightbox();
     sticky();
     utils();
-    map();
     demo();
 
 });
@@ -61,52 +60,13 @@ function openLightBox(before_img_link, after_img_link) {
 }
 
 /* =========================================
- *  sticky header 
+ *  sticky header
  *  =======================================*/
 
 function sticky() {
 
     $(".header").sticky();
 
-}
-
-
-/* =========================================
- *  map 
- *  =======================================*/
-
-function map() {
-
-    var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
-    map = new GMaps({
-        el: '#map',
-        lat: 46.663969,
-        lng: 32.659379,
-        zoomControl: true,
-        zoomControlOpt: {
-            style: 'SMALL',
-            position: 'TOP_LEFT'
-        },
-        panControl: false,
-        streetViewControl: false,
-        mapTypeControl: false,
-        overviewMapControl: false,
-        scrollwheel: false,
-        draggable: false,
-        styles: styles
-    });
-
-    var image = 'img/marker.png';
-
-    map.addMarker({
-        lat: 46.663969,
-        lng: 32.659379,
-        icon: image,
-         title: '',
-         infoWindow: {
-         content: '<p>I`m here.</p>'
-         }
-    });
 }
 
 function utils() {
